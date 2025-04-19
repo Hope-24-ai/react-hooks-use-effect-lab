8afb5dd509408fb088448102353d59256bf37758
hopeyuniawasonga <hope.wasonga@student.moringaschool.com>
2025-04-19 22:54:07 +0300

    added timer to Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);       // reset for next question
+      onAnswered(false);          // auto-answer as incorrect
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId); 
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -30,3 +42,4 @@ function Question({ question, onAnswered }) {
 }
 
 export default Question;
+
